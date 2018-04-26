a44c35cad46cbe2ad7867a1081d17521e3462dbc
seszcze <sebastian@ably.pl>
2018-04-26 22:42:02 +0200

    work in progress

---
+++ src/RoutesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaPress\Routes;
+
+use Illuminate\Support\ServiceProvider;
+
+class RoutesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
