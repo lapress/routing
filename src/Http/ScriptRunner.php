0411e44cda2a1f6c852fd3188482298b3d932b3a
Sebastian Szczepanski <sebastian@ably.pl>
2018-07-18 00:47:55 +0200

    fix admin panel after route error fix

--- src/Http/ScriptRunner.php
+++ src/Http/ScriptRunner.php
@@ -42,13 +42,7 @@ class ScriptRunner
     public function runAdmin($filename, array $globals = [])
     {
         // from wp-settings.php
-        $globals = array_merge($globals, [
-            'wp_version',
-            'wp_db_version',
-            'tinymce_version',
-            'required_php_version',
-            'required_mysql_version',
-        ]);
+        $globals = array_merge($globals, ['wp_version', 'wp_db_version', 'tinymce_version', 'required_php_version', 'required_mysql_version']);
 
         // require current directory is '{$WORDPRESS}/wp-admin/' by 'wp-admin/menu-header.php'
         chdir(wordpress_path('wp-admin'));
@@ -65,27 +59,25 @@ class ScriptRunner
             global ${$global};
         }
 
-//        if (env('APP_ENV') == 'testing1') {
-//            ob_start();
-//
-//            // We'll evaluate the contents of the view inside a try/catch block so we can
-//            // flush out any stray output that might get out before an error occurs or
-//            // an exception is thrown. This prevents any partial views from leaking.
-//            try {
-//                require wordpress_path($path);
-//            } catch (Exception $e) {
-//                ob_end_clean();
-//            }
-//
-//            return ltrim(ob_get_clean());
-//        } else {
         ob_start();
-        require wordpress_path($path);
-        $response = ob_get_contents();
-        ob_end_clean();
 
-        return $response;
+        if (env('APP_ENV') == 'testing1') {
 
-//        }
+
+            // We'll evaluate the contents of the view inside a try/catch block so we can
+            // flush out any stray output that might get out before an error occurs or
+            // an exception is thrown. This prevents any partial views from leaking.
+            try {
+                require wordpress_path($path);
+            } catch (Exception $e) {
+                ob_end_clean();
+            }
+
+            return ltrim(ob_get_clean());
+        } else {
+            require wordpress_path($path);
+
+            ob_end_flush();
+        }
     }
 }
